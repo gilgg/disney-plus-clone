ea4b214f7ad0081c2279c255febfe4543905db29
Gil <limudgil@gmail.com>
2022-01-22 17:01:29 +0200

    added .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,24 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-import { collection, getDocs, getData } from "firebase/firestore";
 import "firebase/compat/storage";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBbS67Z2p6oRZuKearb_fpQPDwBezJPGic",
+//   authDomain: "disney-plus-clone-b4f4e.firebaseapp.com",
+//   projectId: "disney-plus-clone-b4f4e",
+//   storageBucket: "disney-plus-clone-b4f4e.appspot.com",
+//   messagingSenderId: "681090204409",
+//   appId: "1:681090204409:web:e0c1402086bcf75a155828",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBbS67Z2p6oRZuKearb_fpQPDwBezJPGic",
-  authDomain: "disney-plus-clone-b4f4e.firebaseapp.com",
-  projectId: "disney-plus-clone-b4f4e",
-  storageBucket: "disney-plus-clone-b4f4e.appspot.com",
-  messagingSenderId: "681090204409",
-  appId: "1:681090204409:web:e0c1402086bcf75a155828",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
